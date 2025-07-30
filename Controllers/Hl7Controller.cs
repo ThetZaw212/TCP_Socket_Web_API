ca9ab124e7a45220e6c2c59bcd56411f2869e97c
Thet Zaw <kkothet212@gmail.com>
2025-07-30 23:40:52 +0630

    Create Hl7Controller.cs

---
+++ Controllers/Hl7Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TCP_Socket_Web_API.Models;
+using TCP_Socket_Web_API.Services;
+
+namespace TCP_Socket_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Hl7Controller : ControllerBase
+    {
+        [HttpPost("send")]
+        [EndpointSummary("Send Message To LIS.")]
+        public async Task<IActionResult> Send([FromBody] Hl7OrderRequest request)
+        {
+            string hl7Message = Hl7TemplateBuilder.BuildFromOrder(request);
+
+            var sender = new Hl7TcpSenderService("192.168.100.114", 5000); 
+            bool sent = await sender.SendMessageAsync(hl7Message);
+
+            return sent ? Ok("HL7 sent successfully.") : StatusCode(500, "Failed to send HL7.");
+        }
+    }
+}
