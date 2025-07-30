2e09cb3530eb71b0ea4b4215071262a45b48c654
Thet Zaw <kkothet212@gmail.com>
2025-07-31 00:06:35 +0630

    Create Hl7TcpSenderService.cs

---
+++ Services/Hl7TcpSenderService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Socket_Web_API.Services
+{
+    public class Hl7TcpSenderService
+    {
+        private readonly string _ip;
+        private readonly int _port;
+
+        public Hl7TcpSenderService(string ip, int port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        public async Task<bool> SendMessageAsync(string hl7Message)
+        {
+            try
+            {
+                using TcpClient client = new TcpClient();
+                await client.ConnectAsync(_ip, _port);
+
+                using NetworkStream stream = client.GetStream();
+
+                string mllpMessage = $"\x0B{hl7Message}\x1C\r";
+                byte[] bytes = Encoding.UTF8.GetBytes(mllpMessage);
+
+                await stream.WriteAsync(bytes, 0, bytes.Length);
+
+                // Optionally read ACK
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("ACK from LIS: " + response);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HL7 Send Error: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
