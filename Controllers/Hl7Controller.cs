fb8772de4690cd82e0994cc6d9a757ace41c2d38
Ko Thet <kkothet212@gmail.com>
2025-08-22 09:41:51 +0630

    Update Hl7Controller.cs

--- Controllers/Hl7Controller.cs
+++ Controllers/Hl7Controller.cs
@@ -15,10 +15,24 @@ namespace TCP_Socket_Web_API.Controllers
         {
             string hl7Message = Hl7TemplateBuilder.BuildFromOrder(request);
 
-            var sender = new Hl7TcpSenderService("192.168.100.253", 5050); 
+            var sender = new Hl7TcpSenderService("192.168.100.253", 20860); 
             bool sent = await sender.SendMessageAsync(hl7Message);
 
             return sent ? Ok("HL7 sent successfully.") : StatusCode(500, "Failed to send HL7.");
         }
+
+        [HttpPost("cancel")]
+        [EndpointSummary("Send Cancel Message To LIS.")]
+        public async Task<IActionResult> Cancel([FromBody] Hl7OrderRequest request)
+        {
+            // Build cancel HL7 message
+            string hl7Message = Hl7TemplateBuilder.BuildCancelOrder(request);
+
+            var sender = new Hl7TcpSenderService("192.168.100.253", 20860);
+            bool sent = await sender.SendMessageAsync(hl7Message);
+
+            return sent ? Ok("HL7 cancel sent successfully.") : StatusCode(500, "Failed to send HL7 cancel.");
+        }
+
     }
 }
