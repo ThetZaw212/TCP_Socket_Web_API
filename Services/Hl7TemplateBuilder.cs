f24c70a3fd79dabd278f790c57cbeeb038e91873
Ko Thet <kkothet212@gmail.com>
2025-08-22 09:41:49 +0630

    Update Hl7TemplateBuilder.cs

--- Services/Hl7TemplateBuilder.cs
+++ Services/Hl7TemplateBuilder.cs
@@ -23,5 +23,29 @@ namespace TCP_Socket_Web_API.Services
 
             return string.Join("\r", new[] { msh, pid, pv1, orc }.Concat(obrs));
         }
+
+        public static string BuildCancelOrder(Hl7OrderRequest request)
+        {
+            string timestamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string msgId = Guid.NewGuid().ToString("N");
+
+            string msh = $"MSH|^~\\&|Ber|VH|LAB|VH|{timestamp}||OML^O21^OML_O21|{msgId}|P|2.5||||AL|||||";
+            string pid = $"PID|1||{request.PatientId}||^{request.PatientName}||{request.DOB}|{request.Gender}|||{request.Address}|MMR||||||||||||||||||N";
+            string pv1 = $"PV1|1|O|OUTP||||{request.DoctorId}^{request.DoctorName}||||||||||||O|||||||||||||||||||||||||{request.OrderTime}|";
+
+            // ORC-1 = XO → Cancel
+            string orc = $"ORC|XO|{request.OrderNumber}|{request.DoctorId}^{request.DoctorName}|CA|||{request.SampleType}||{request.OrderTime}|{request.CollectionTime}";
+
+            // You *may* need at least one OBR to specify which test(s) to cancel
+            var obrs = new List<string>();
+            for (int i = 0; i < request.TestCodes.Count; i++)
+            {
+                string testCode = request.TestCodes[i];
+                obrs.Add($"OBR|{i + 1}|||{testCode}||{request.OrderTime}|{request.OrderTime}||||A|||{request.OrderTime}");
+            }
+
+            return string.Join("\r", new[] { msh, pid, pv1, orc }.Concat(obrs));
+        }
+
     }
 }
